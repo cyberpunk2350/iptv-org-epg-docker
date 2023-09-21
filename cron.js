719d5ad60b5f36dbb17642bde98eed9e6192c712
Henry Rice <9151488+cyberpunk2350@users.noreply.github.com>
2023-09-21 18:05:25 -0400

    Update cron.js
    changed sites

--- cron.js
+++ cron.js
@@ -4,25 +4,25 @@ var cron = require("node-cron");
 const execute = () => {
   console.log("Grabbing epg...");
   try {
-    execSync("SITE=tvhebdo.com npm run grab", {
+    execSync("SITE=i.mjh.nz npm run grab", {
       stdio: "inherit",
       cwd: "/usr/src/app",
     });
   } catch (error) {
     console.error(
-      "something went wront while grabbing epg from tvhebdo",
+      "something went wront while grabbing epg from i.mjh.nz",
       error
     );
   }
 
   try {
-    execSync("SITE=tvpassport.com npm run grab", {
+    execSync("SITE=tvguide.com npm run grab", {
       stdio: "inherit",
       cwd: "/usr/src/app",
     });
   } catch (error) {
     console.error(
-      "something went wront while grabbing epg from tvpassport",
+      "something went wront while grabbing epg from tvguide.com",
       error
     );
   }
