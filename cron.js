706e792219461c3d54c8f17931cf593bc6c8d7e2
Henry Rice <9151488+cyberpunk2350@users.noreply.github.com>
2023-09-24 00:01:52 -0400

    Update cron.js
    adding a few sites

--- cron.js
+++ cron.js
@@ -4,17 +4,41 @@ var cron = require("node-cron");
 const execute = () => {
   console.log("Grabbing epg...");
   try {
+    execSync("SITE=tvpassport.com npm run grab", {
+      stdio: "inherit",
+      cwd: "/usr/src/app",
+    });
+  } catch (error) {
+    console.error(
+      "something went wrong while grabbing epg from tvpassport.com",
+      error
+    );
+  }
+  
+  try {
     execSync("SITE=i.mjh.nz npm run grab", {
       stdio: "inherit",
       cwd: "/usr/src/app",
     });
   } catch (error) {
     console.error(
-      "something went wront while grabbing epg from i.mjh.nz",
+      "something went wrong while grabbing epg from i.mjh.nz",
       error
     );
   }
-
+  
+    try {
+    execSync("SITE=i.mjh.nz_pluto npm run grab", {
+      stdio: "inherit",
+      cwd: "/usr/src/app",
+    });
+  } catch (error) {
+    console.error(
+      "something went wrong while grabbing epg from i.mjh.nz_pluto",
+      error
+    );
+  }
+  
   try {
     execSync("SITE=tvguide.com npm run grab", {
       stdio: "inherit",
@@ -22,7 +46,7 @@ const execute = () => {
     });
   } catch (error) {
     console.error(
-      "something went wront while grabbing epg from tvguide.com",
+      "something went wrong while grabbing epg from tvguide.com",
       error
     );
   }
